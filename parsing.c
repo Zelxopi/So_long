291edaa4c8eb1d8fdf8228a24f0b3d87951294c7
Maya Tremblay <zelxopi@Lilbee.local>
2022-07-05 15:38:19 -0400

    completed parsing let's put it up to norm

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: mtrembla <mtrembla@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 13:32:18 by mtrembla          #+#    #+#             */
-/*   Updated: 2022/07/04 16:33:31 by mtrembla         ###   ########.fr       */
+/*   Updated: 2022/07/05 15:25:59 by mtrembla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ int	ft_map_is_square(parsing *map)
 	close(fd);
 	fd = open(map->mapfile, O_RDONLY);
 	len = ft_strlen(ft_gnl(fd));
+	map->map_len = len;
 	while(--line)
 	{
 		if (ft_strlen(ft_gnl(fd)) != len)
@@ -101,12 +102,58 @@ int	ft_topandbottom(parsing *map)
 		ft_error("Check tes murs du bas");
 		str++;
 	}
+	close(fd);
 	return(1);
 }
 
 int	ft_firstandlast(parsing *map)
 {
-	
+	int	fd;
+	char *str;
+	int	heightcpy;
+
+	heightcpy = map->map_height;
+	fd = open(map->mapfile, O_RDONLY);
+	ft_gnl(fd);
+	while(heightcpy > 2)
+	{	
+		str = ((char *)ft_gnl(fd));
+		if (*str != '1')
+		ft_error("yo tes murs");
+		while(*str)
+			str++;
+		str--;
+		if (*str != '1')
+		ft_error("yo tes murs");
+		heightcpy--;
+	}
+	close(fd);
+	return(1);
+}
+
+int	ft_mapname(parsing *map)
+{
+	char *str;
+	char *ber;
+	int fd;
+
+	fd = open(map->mapfile, O_RDONLY);
+	if (fd == -1)
+	ft_error("mauvais nom de map");
+	ber = ".ber";
+	str = ft_strrchr(map->mapfile, '.');
+	if(ft_strncmp(str, ber, 7) != 0)
+		ft_error("Ça a pas l'air d'un '.ber' ton affaire");
+	return(1);
+}
+
+int ft_parsing(parsing *map)
+{
+	if (ft_mapname(map) == 1 && ft_map_is_square(map) == 1 &&
+	ft_peandc(map) == 1 && ft_topandbottom(map) == 1 && ft_firstandlast(map) == 1)
+	return(1);
+	else
+	return(-1);
 }
 
 int main(int	argc, char **argv)
@@ -117,7 +164,10 @@ int main(int	argc, char **argv)
 	map = malloc(sizeof(parsing));
 
 	map->mapfile = argv[1];
+	/*printf("%d\n", ft_mapname(map));
 	printf("%d\n", ft_map_is_square(map));
 	printf("%d\n", ft_peandc(map));
 	printf("%d\n", ft_topandbottom(map));
-}
+	printf("%d\n", ft_firstandlast(map));*/
+	printf("%d\n", ft_parsing(map));
+	}
